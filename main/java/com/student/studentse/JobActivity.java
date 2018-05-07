fc9f4a333098e1246bbc8bf02b387c38bed60acf
StaneCobalt <scsladek91@gmail.com>
2018-05-06 20:59:14 -0500

    Fix: null pointer
    null pointer on job activity for button click listener was resolved. minor code clean up here and there was done to make things more consistent for our min support of api 15. Issue concerning JSONobject in postexecute found.

--- main/java/com/student/studentse/JobActivity.java
+++ main/java/com/student/studentse/JobActivity.java
@@ -1,5 +1,6 @@
-package com.student.studentse.myapplication;
+package com.student.studentse;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
@@ -19,15 +20,16 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class JobActivity extends AppCompatActivity {
+public class JobActivity extends AppCompatActivity implements View.OnClickListener {
     JSONArray jsonArray;
     JSONObject jsonObj;
     URL url;
-    String baseUrl = "http://jobs.github.com/positions.json?description=", loc, field, jTitle, jLoc, Jtype, Jdescription, jUrl, jCreated;
+    String baseUrl = "https://jobs.github.com/positions.json?description=", loc, field, jTitle, jLoc, Jtype, Jdescription, jUrl, jCreated;
     EditText locEditText, fieldEditText;
 
     @Override
@@ -37,34 +39,53 @@ public class JobActivity extends AppCompatActivity {
 
         locEditText = findViewById(R.id.locationEditText);
         fieldEditText = findViewById(R.id.fieldEditText);
-        Button jobButton = (Button) findViewById(R.id.btnJobs);
-        jobButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loc = locEditText.getText().toString();
-                field = fieldEditText.getText().toString();
-                loc = loc.replace(' ', '+');
-                baseUrl += field + "&location=" + loc;
-                try {
-                    url = new URL(baseUrl);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                if (url != null) {
-                    GetJobsTask getJobsTask = new GetJobsTask();
-                    getJobsTask.execute(url);
-                } else {
-                    Toast.makeText(getApplicationContext(), R.string.invalid_url, Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
+        Button jobButton = (Button) findViewById(R.id.buttonJob);
+        jobButton.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v){
+        loc = locEditText.getText().toString();
+        field = fieldEditText.getText().toString();
+        if(!loc.isEmpty()) {
+            loc = loc.replace(' ', '+');
 
-
+            baseUrl += field + "&location=" + loc;
+            try {
+                url = new URL(baseUrl);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (url != null) {
+                GetJobsTask getJobsTask = new GetJobsTask();
+                getJobsTask.execute(url);
+            } else {
+                Toast.makeText(getApplicationContext(), R.string.invalid_url, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     private class GetJobsTask extends AsyncTask<URL, Void, JSONObject> {
+
+        String streamToString(InputStream stream) throws IOException{
+            StringBuilder result = new StringBuilder();
+            try {
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(stream));
+                String data;
+
+                while ((data = reader.readLine()) != null) {
+                    result.append(data);
+                }
+            }
+            catch(IOException e){
+                //Toast.makeText(getApplicationContext(),R.string.read_error,Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+            stream.close();
+            return result.toString();
+        }
+
         @Override
         protected JSONObject doInBackground(URL... params) {
             HttpURLConnection connection = null;
@@ -74,42 +95,35 @@ public class JobActivity extends AppCompatActivity {
                 int response = connection.getResponseCode();
 
                 if (response == HttpURLConnection.HTTP_OK) {
-                    StringBuilder builder = new StringBuilder();
-
-                    try (BufferedReader reader = new BufferedReader(
-                            new InputStreamReader(connection.getInputStream()))) {
-
-                        String line;
-
-                        while ((line = reader.readLine()) != null) {
-                            builder.append(line);
-                        }
+                    //StringBuilder builder = new StringBuilder();
+                    try{
+                        String responseString = streamToString(connection.getInputStream());
+                        return new JSONObject(responseString);
                     }
                     catch (IOException e) {
-                        Toast.makeText(getApplicationContext(),R.string.read_error,Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(getApplicationContext(),R.string.read_error,Toast.LENGTH_SHORT).show();
                         e.printStackTrace();
                     }
-
-                    return new JSONObject(builder.toString());
+                    return null;
+                    //return new JSONObject(builder.toString());
                 }
-                else {
-                    Toast.makeText(getApplicationContext(),R.string.connect_error,Toast.LENGTH_SHORT).show();
-                }
+                //else Toast.makeText(getApplicationContext(),R.string.connect_error,Toast.LENGTH_SHORT).show();
             }
             catch (Exception e) {
-                Toast.makeText(getApplicationContext(),R.string.connect_error,Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(),R.string.connect_error,Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             }
             finally {
                 connection.disconnect();
             }
-
             return null;
         }
 
         @Override
-        protected void onPostExecute(JSONObject article) {
+        protected void onPostExecute(JSONObject article)
+        {
             convertJSONtoArrayList(article);
+            newActivityFunction();
         }
     }
     private void newActivityFunction(){
@@ -124,6 +138,7 @@ public class JobActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+    @TargetApi(19)
     private void convertJSONtoArrayList(JSONObject response) {
 
         try {
--- main/java/com/student/studentse/MainActivity.java
+++ main/java/com/student/studentse/MainActivity.java
@@ -1,4 +1,4 @@
-package com.student.studentse.myapplication;
+package com.student.studentse;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
